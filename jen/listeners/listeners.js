21fde349db78304009f2d9da5efcd96e98638569
James Barzegar <james.barzegar@gmail.com>
2016-10-16 20:19:35 -0400

    Bot now deletes in half voice

--- jen/listeners/listeners.js
+++ jen/listeners/listeners.js
@@ -1,11 +1,17 @@
 const client = require('../conf/client');
 const { logger } = require('../conf/config');
+const getConnectedClients = require('../conf/get-connected-clients');
 
 // Logs for ready state
 client.on('ready', () => {
   logger.info('Client ready');
+  getConnectedClients();
 });
 
 client.on('message', message => {
-  require('./on_message')(message, client);
+  require('./on_message')(message, client, getConnectedClients);
+});
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  getConnectedClients();
 });
--- jen/listeners/on_message.js
+++ jen/listeners/on_message.js
@@ -1,22 +1,36 @@
 const { logger } = require('../conf/config');
-const Promise = require('bluebird');
 
-function checkMessage(message, client) {
-  const content = message.content.toLowerCase();
-
-  // Check if the phrase 'you people' is in the string
-  if (content.indexOf('you people') !== -1) {
-    // Compare message to make sure that the bot won't keep sending messages
-    if (message.author.id !== client.user.id) {
-      return message.reply('Fuck you mean you people')
-        .then(msg => logger.info(`Reply sent to ${msg.author.id}`))
-        .catch(err => logger.warn(err));
+function checkMessage(message, client, channels) {
+  const voiceChannels = channels();
+  // Check to see if user typed in channel where half voice chat rule is enforced
+  if (message.channel.id === '237325959395672064') {
+    // Check if anyone is in a voice channel
+    if (voiceChannels.length === 0) {
+      console.log('No one is in voice');
+      // Delete users message and send reminder
+      message.delete()
+        .then(msg => logger.info(`Deleted message from ${msg.author.id}`))
+        .catch(err => logger.error(err));
+      message.reply('You can\'t talk here unless you are connected to a voice channel');
+    } else {
+      // If there are users connected to a voice channel check if author is in one themselves
+      for (let i in voiceChannels) {
+        const user = voiceChannels[i].members.find('id', message.author.id);
+        if (user !== null) {
+          return console.log('User is in a voice channel');
+        } else {
+          if (message.author.id !== client.user.id) {
+            message.delete()
+              .then(msg => logger.info(`Deleted message from ${msg.author.id}`))
+              .catch(err => logger.error(err));
+            message.reply('You can\'t talk here unless you are connected to a voice channel');
+          }
+        }
+      }
     }
-  } else {
-    return logger.error('Not found');
   }
 }
 
-module.exports = (message, client) => {
-  checkMessage(message, client);
+module.exports = (message, client, channels) => {
+  checkMessage(message, client, channels);
 };
