fd9a2808e1c96af7570b20eec9f712c21abe87a8
James Barzegar <james.barzegar@gmail.com>
2016-10-29 13:57:10 -0400

    Added yumi

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,14 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
 
-  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  # MongoDB
+  config.vm.network "forwarded_port", guest: 27017, host: 27017
+  config.vm.network "forwarded_port", guest: 27018, host: 27018
+  config.vm.network "forwarded_port", guest: 27019, host: 27019
+  config.vm.network "forwarded_port", guest: 28017, host: 28017
+
 
   config.vm.network "private_network", ip: "192.168.33.10"
+
+  config.vm.provision "shell", path: "yumi/provision.sh", privileged: false
 end
---
+++ yumi/provision.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Updating packages..."
+
+sudo apt-get update -qq > /dev/null
+
+sudo apt-get upgrade -qq -y > /dev/null
+
+sudo apt-get dist-upgrade -qq -y > /dev/null
+
+sudo apt-get autoremove -y
+
+echo "Install mongodb"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update
+
+sudo apt-get install -qq -y \
+    git \
+    curl \
+    mongodb \
+    > /dev/null
+
+sudo service mongodb start
