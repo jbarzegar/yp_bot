40c4f260a2743189e33bfe3a0f1a26c3e02831c2
James Barzegar <james.barzegar@gmail.com>
2016-10-30 19:16:39 -0400

    RIP ALL OF THE TIME

--- eve/config/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  MONGODB: {
-    URL: process.env.MONGO_URL || 'mongodb://localhost/test'
-  },
-  secret: 'nwqg$4z#vfi5q*cz^r#l$6o94hl9bgn!l2%$ww-nrfgpxv)=*2' || process.env.JEN_SECRET
-};
-
-module.exports = config;
