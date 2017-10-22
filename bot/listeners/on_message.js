d70f19fa976640b300738d30f7f8835fad2210f3
James Barzegar <james.barzegar@gmail.com>
2017-10-22 12:11:11 -0400

    Added anti self promo

--- bot/listeners/on_message.js
+++ bot/listeners/on_message.js
@@ -1,15 +1,17 @@
 // Handlers
 const halfVoicers = require('../handlers/half_voicers');
 const youPeople = require('../handlers/you_people');
+const antiSelfPromo = require('../handlers/anti_self_promo');
 
 // Commands
 const define = require('../commands/define');
 
 // Start all
-module.exports = (m) => {
+module.exports = m => {
   // Handlers
   halfVoicers(m);
   youPeople(m);
+  antiSelfPromo(m);
   // Commands
   define(m);
 };
